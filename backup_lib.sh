e0883f26e30efca4f22808265a48a5ba7f6301f3
Herve Nicol <herve.nicol@gmail.com>
2018-10-25 23:06:48 +0200

    fix bashate alerts

--- backup_lib.sh
+++ backup_lib.sh
@@ -4,36 +4,31 @@ mount_flag_basename=/tmp/mount_flag
 my_mount_flag_filename=$mount_flag_basename.$$
 backup_part=/media/backup_disk
 
-function set_mount_flag
-{
-	touch $my_mount_flag_filename
+function set_mount_flag {
+    touch $my_mount_flag_filename
 #        echo "mount flag set"
 }
 
-function is_mount_flag_set
-{
-	if ls /tmp/mount_flag* >/dev/null 2>&1; then
-#		echo "Mount flag is set"
-		return 0
-	fi
-	return 1
+function is_mount_flag_set {
+    if ls /tmp/mount_flag* >/dev/null 2>&1; then
+#        echo "Mount flag is set"
+        return 0
+    fi
+    return 1
 }
 
-function unset_mount_flag
-{
-	rm $my_mount_flag_filename
-	is_mount_flag_set && return 1
-#	echo mount flag unset
-	return 0
+function unset_mount_flag {
+    rm $my_mount_flag_filename
+    is_mount_flag_set && return 1
+#    echo mount flag unset
+    return 0
 }
 
-function writemount_backup
-{
-	is_mount_flag_set || mount -o remount,rw $backup_part
-	set_mount_flag
+function writemount_backup {
+    is_mount_flag_set || mount -o remount,rw $backup_part
+    set_mount_flag
 }
 
-function readonlymount_backup
-{
-	unset_mount_flag && mount -o remount,ro $backup_part
+function readonlymount_backup {
+    unset_mount_flag && mount -o remount,ro $backup_part
 }
--- snapshot_rvsrv_share_week.sh
+++ snapshot_rvsrv_share_week.sh
@@ -8,7 +8,7 @@ backup_name=/media/backup_disk/backup_rvsrv/share
 writemount_backup
 rm -rf "$backup_name".week.4
 for iteration in {4..2}; do
-	mv "$backup_name.week.$(( "$iteration"-1 ))" "$backup_name.week.$iteration"
+    mv "$backup_name.week.$(( "$iteration"-1 ))" "$backup_name.week.$iteration"
 done
 cp -al "$backup_name" "$backup_name".week.1
 readonlymount_backup
